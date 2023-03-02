6672cfa1eceb50c59db66a52c2c4df2eb0755d2b
Linzi <1151953214@qq.com>
2023-03-02 23:40:18 +0800

    增加注释

--- testcases/test_login.py
+++ testcases/test_login.py
@@ -9,12 +9,17 @@ class TestLogin:
 
     def test_login(self):
         driver = DriverConfig().driver_config()
+        # 登录系统
         driver.get("http://www.tcpjwtester.top")
         sleep(3)
+        # 输入用户名
         LonginPage().login_input_value(driver,"用户名","周杰伦")
         sleep(3)
+        # 输入密码
         LonginPage().login_input_value(driver, "密码", "123456")
         sleep(3)
+        # 点击登录
         LonginPage().click_login(driver,"登录")
         sleep(3)
+        # 退出浏览器
         driver.quit()
