2f9ff47ec9ee542198c1fa7d206960c38fb34e68
Linzi <1151953214@qq.com>
2023-03-08 22:37:21 +0800

    等待元素页面完全加载完成方法

--- base/ObjectMap.py
+++ base/ObjectMap.py
@@ -2,7 +2,8 @@
 
 import time
 
-from selenium.common.exceptions import ElementNotVisibleException
+from selenium.common.exceptions import ElementNotVisibleException, WebDriverException
+
 
 class ObjectMar:
     def element_get(self, driver, locate_type, locator_expresson, timeout=10, must_be_visible=False):
@@ -39,4 +40,36 @@ class ObjectMar:
                     break
                 pass
             time.sleep(0.1)
-        raise ElementNotVisibleException("元素定位失败，定位方式："+locate_type+"定位表达式："+locator_expresson)
+        raise ElementNotVisibleException("元素定位失败，定位方式：" + locate_type + "定位表达式：" + locator_expresson)
+
+    def wait_for_ready_state_complete(self, driver, timeout=30):
+        '''
+        等待页面完全加载完成
+        :param driver:浏览器驱动
+        :param timeout:超时时间
+        :return:
+        '''
+        # 开始时间
+        start_ms = time.time() * 1000
+        # 设置的结束时间
+        stop_ms = start_ms + (timeout * 1000)
+        for x in range(int(timeout * 10)):
+            try:
+                # 获取页面的状态
+                ready_state = driver.execute_script("return document.readyState")
+
+            except WebDriverException:
+                # 如果有driver的错误，执行js会失败，就直接跳过
+                time.sleep(0.03)
+                return True
+            #     如果页面元素全部加载完成返回true
+            if ready_state == "complete":
+                time.sleep(0.01)
+                return True
+            else:
+                now_ms = time.time() * 1000
+                # 如果超时了就break
+                if now_ms >= stop_ms:
+                    break
+                time.tzset(0.1)
+        raise Exception("打开网页时，页面元素在%s秒后仍然没有完全加载完成" % timeout)
