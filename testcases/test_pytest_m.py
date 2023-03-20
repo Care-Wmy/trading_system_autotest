b381d2f3867a64013ed1c63942b47d54af150eba
Linzi <1151953214@qq.com>
2023-03-20 21:26:11 +0800

    fixture用法

--- testcases/test_pytest_m.py
+++ testcases/test_pytest_m.py
@@ -13,7 +13,7 @@ class TestPytestMClass:
         return getdriver
 
     @pytest.mark.bing
-    def test_open_bing(self,driver):
+    def test_open_bing(self, driver):
         # driver = DriverConfig().driver_config()
         driver.get("https://cn.bing.com")
         sleep(3)
@@ -21,14 +21,14 @@ class TestPytestMClass:
 
     @pytest.mark.baidu
     # pytest -m baidu 在命令行输入命令 就会运行这个用例
-    def test_open_baidu(self,driver):
+    def test_open_baidu(self, driver):
         # driver = DriverConfig().driver_config()
         driver.get("https://baidu.com")
         sleep(3)
         driver.quit()
 
     @pytest.mark.google
-    def test_open_google(self,driver):
+    def test_open_google(self, driver):
         # driver = DriverConfig().driver_config()
         driver.get("https://fanyi.baidu.com")
         sleep(3)
