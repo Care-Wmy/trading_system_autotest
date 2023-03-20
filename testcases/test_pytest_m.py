7ee7f1e32877c1469470b4de33398ef0f951115d
Linzi <1151953214@qq.com>
2023-03-20 21:25:19 +0800

    fixtrue用法

--- testcases/test_pytest_m.py
+++ testcases/test_pytest_m.py
@@ -6,24 +6,30 @@ from config.driver_config import DriverConfig
 
 
 class TestPytestMClass:
+
+    @pytest.fixture()
+    def driver(self):
+        getdriver = DriverConfig().driver_config()
+        return getdriver
+
     @pytest.mark.bing
-    def test_open_bing(self):
-        driver = DriverConfig().driver_config()
+    def test_open_bing(self,driver):
+        # driver = DriverConfig().driver_config()
         driver.get("https://cn.bing.com")
         sleep(3)
         driver.quit()
 
     @pytest.mark.baidu
     # pytest -m baidu 在命令行输入命令 就会运行这个用例
-    def test_open_baidu(self):
-        driver = DriverConfig().driver_config()
+    def test_open_baidu(self,driver):
+        # driver = DriverConfig().driver_config()
         driver.get("https://baidu.com")
         sleep(3)
         driver.quit()
 
     @pytest.mark.google
-    def test_open_google(self):
-        driver = DriverConfig().driver_config()
+    def test_open_google(self,driver):
+        # driver = DriverConfig().driver_config()
         driver.get("https://fanyi.baidu.com")
         sleep(3)
         driver.quit()
