653edcfffdba1496baa3d147f17adf07e1827c22
Linzi <1151953214@qq.com>
2023-03-20 23:19:55 +0800

    pytest运用parametrize进行参数化

--- testcases/test_add_goods.py
+++ testcases/test_add_goods.py
@@ -2,13 +2,38 @@
 
 from time import sleep
 
+import pytest
+
 from page.LoginPage import LonginPage
 from page.LeftMenuPage import LeftMenuPage
 from page.GoodsPage import GoodsPage
 
-class TestAddGoods:
+goods_info_list = [
+    {
+        "goods_title": "新增商品测试01",
+        "goods_details": "商品详情测试",
+        "goods_num": 1,
+        # goods_pic_list=["hhh.jpg"],
+        "goods_price": 111,
+        "goods_status": "上架",
+        "bottom_button_name": "提交"
+    },
+    {
+        "goods_title": "新增商品测试02",
+        "goods_details": "商品详情测试",
+        "goods_num": 1,
+        # goods_pic_list=["hhh.jpg"],
+        "goods_price": 222,
+        "goods_status": "上架",
+        "bottom_button_name": "提交"
+    }
 
-    def test_add_goods_001(self,driver):
+]
+
+
+class TestAddGoods:
+    @pytest.mark.parametrize("goods_info", goods_info_list)
+    def test_add_goods_001(self, driver, goods_info):
         # driver = DriverConfig().driver_config()
         # 登录
         LonginPage().login(driver, "jay")
@@ -20,12 +45,12 @@ class TestAddGoods:
         sleep(2)
         GoodsPage().add_new_goods(
             driver,
-            goods_title="新增商品测试11",
-            goods_details="商品详情测试",
-            goods_num=1,
+            goods_title=goods_info["goods_title"],
+            goods_details=goods_info["goods_details"],
+            goods_num=goods_info["goods_num"],
             # goods_pic_list=["hhh.jpg"],
-            goods_price=123,
-            goods_status="上架",
-            bottom_button_name="提交"
+            goods_price=goods_info["goods_price"],
+            goods_status=goods_info["goods_status"],
+            bottom_button_name=goods_info["bottom_button_name"]
         )
         sleep(3)
--- testcases/test_search_order_buy.py
+++ testcases/test_search_order_buy.py
@@ -3,22 +3,23 @@
 
 from time import sleep
 
-from config.driver_config import DriverConfig
+import pytest
+
 from page.LeftMenuPage import LeftMenuPage
 from page.LoginPage import LonginPage
 from page.OrderPage import OrderPage
 
+tab_list = ["全部", "待付款", "待发货", "运输中", "待确认", "待评价"]
+
 
 class TestOrderBuy:
-    def test_order_buy(self,driver):
+    @pytest.mark.parametrize("tab", tab_list)
+    def test_order_buy(self, driver,tab):
         # driver = DriverConfig().driver_config()
         LonginPage().login(driver, "jay")
         LeftMenuPage().click_level_one_menu(driver, "我的订单")
         sleep(1)
         LeftMenuPage().click_level_two_menu(driver, "已买到的宝贝")
         sleep(2)
-        tab_list = ["全部", "待付款", "待发货", "运输中", "待确认", "待评价"]
-        for tab in tab_list:
-            OrderPage().click_order_tab(driver, tab)
-            sleep(2)
+        OrderPage().click_order_tab(driver, tab)
         # driver.quit()
